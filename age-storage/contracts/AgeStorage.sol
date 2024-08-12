cdb7fd73c8b0332bec203d947f076a14aaa6db6c
Spencer Miller <spencer.github.123@gmail.com>
2024-08-12 17:48:59 +0300

    add DPF modifier to contract functions

--- age-storage/contracts/AgeStorage.sol
+++ age-storage/contracts/AgeStorage.sol
@@ -6,17 +6,21 @@ import "./DataPrivacyFramework.sol";
 contract AgeStorage is DataPrivacyFramework {
     uint256 private _age;
 
+    /**
+     * @notice by default, all users are allowed to execute all operations
+     * @dev see DataPrivacyFramework contract methods for updating permissions
+     */
     constructor() DataPrivacyFramework(false, false) {}
 
-    function getAge() public view returns (uint256) {
+    function getAge() public view onlyAllowedUserOperation("op_get_age") returns (uint256) {
         return _age;
     }
 
-    function isGreaterThan(uint256 age) public view returns (bool) {
+    function isGreaterThan(uint256 age) public view onlyAllowedUserOperation("op_is_greater_than") returns (bool) {
         return _age > age;
     }
 
-    function setAge(uint256 age) public {
+    function setAge(uint256 age) public onlyAllowedUserOperation("op_set_age") {
         _age = age;
     }
 }
